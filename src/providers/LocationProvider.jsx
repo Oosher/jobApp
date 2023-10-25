fb7ae93876b32be91a5f60e74ef6500bb2e13a24
Oosher <105375761+Oosher@users.noreply.github.com>
2023-10-26 02:00:51 +0300

    fixed async bugs

--- src/providers/LocationProvider.jsx
+++ src/providers/LocationProvider.jsx
@@ -32,7 +32,7 @@ export default function LocationProvider({children}) {
             
             const countryData =  await getCountryByGeolocation(geolocation?.latitude.toFixed(1),geolocation?.longitude.toFixed(1));
             
-            setSearch({label:countryData.Country.LocalizedName,locationKey:countryData?.Key})
+            setSearch({label:countryData?.Country?.LocalizedName,locationKey:countryData?.Key})
             
         }
     }
